1ac515ff0932f3c3a95f6878cd6b63cf2e70e01f
Jonathan Alvarado <71190136+jona1403@users.noreply.github.com>
2023-06-05 14:32:45 -0600

    Update server.py
    Se agregó una doble chequeo si ya se tiene un objeto con el mismo id dentro del arreglo.
    Se optimizó la api al utilizar la libreria concurrent.futures que permite realizar las peticiones de forma concurrente y paralela, permitiendo la asincronia dentro de la misma, por lo que el tiempo de .

--- Python_Server/server.py
+++ Python_Server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, make_response
 from waitress import serve
 import requests
-
+import concurrent.futures
 
 #la api está hecha con flask
 app = Flask(__name__)
@@ -11,12 +11,14 @@ ENDPOINT = 'https://api.chucknorris.io/jokes/random'
 
 #Funcion auxiliar para la verificacion de la existencia del objeto por el id
 def exists(objects, obj):
-
     #Iteracion del arreglo
     for object in objects:
         #Condicion de existencia del objeto por el ID
         if object["id"] == obj["id"]:
-            return True
+            #Doble chequeo al encontrar un objeto con el mimos ID
+            for ob in objects:
+                if ob["id"] == obj["id"]:
+                    return True
     return False
 
 #Endpoint root de prueba para verificar que la api corra correctamente
@@ -26,29 +28,33 @@ def root():
     return '<h1>Listening on port 3000</h1>'
 
 
+# Función para obtener un objeto a partir de una URL
+def peticionObjeto(url):
+    response = requests.get(url)
+    return response.json()
+
+
 #Endpoint tipo get para la obtención de objetos
 @app.route('/getObjects', methods= {'GET'})
 def getObjects():
     #Arreglo donde almacenaremos los 25 objetos
     objects = []
 
-    #Loop para iterar hasta que el arreglo objetos tenga una dimension de 25
-    while len(objects) < 25:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        while len(objects) < 25:
+            # Crear una lista de futures para las solicitudes de objetos faltantes
+            futures = [executor.submit(peticionObjeto, ENDPOINT) for _ in range(25 - len(objects))]
 
-        #Objeto recivido por el enpoint anteriormente declarado
-        object_received = requests.get(ENDPOINT)
-    
-        #object_received en formato JSON
-        json_object = object_received.json()
-        
-        #Condición para que sea un objeto no existente dentro del arreglo 
-        if not exists(objects ,json_object):
-            #Se agrega un objeto mas al arreglo
-            objects.append(json_object)
+            # Esperar a que se completen las solicitudes y obtener los resultados
+            for future in concurrent.futures.as_completed(futures):
+                json_object = future.result()
+                 #Condición para que sea un objeto no existente dentro del arreglo 
+                if not exists(objects ,json_object):
+                    #Se agrega un objeto mas al arreglo
+                    objects.append(json_object)
 
     #Se establece la respuesta que envieremos con el codigo de ok
     response = make_response(objects, 200)
-
     return response
 
 
